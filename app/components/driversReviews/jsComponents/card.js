a0cbed09330108a72c09278d0776afb4bd808e20
Nikita Korjavin <nik.korg94@gmail.com>
2018-07-18 16:14:15 +0300

    добавил нижние точки-переключатели + стейт driversReviews+хендлы кликов по точкам-переключателям

--- app/components/driversReviews/jsComponents/card.js
+++ app/components/driversReviews/jsComponents/card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import createMarkup from 'components/commonlyUsedComponents/createMarkup/createMarkup';
 
-const Card = ({ content: { cardNum, description, name } }) => (
+const Card = ({ content: { name, description, cardNum } }) => (
   <div className={`card ${cardNum}`}>
     <div className="img-box" />
     <div className="text-box">
